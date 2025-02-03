a0650c474642ecb29173b52414816ef1de41e439
Lukas Nackmayr <lukas.nackmayr@student.kdg.be>
2025-02-03 14:54:58 +0100

    feat: simple go webserver as blueprint for chat app

---
+++ go.mod
@@ -0,0 +1,3 @@
+module kdg/be/lab
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "log"
+  "net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
+  w.Write([]byte("Webserver"))
+}
+
+func main() {
+  mux := http.NewServeMux()
+  mux.HandleFunc("/", home)
+
+  log.Print("Starting server on :4000")
+  err := http.ListenAndServe(":4000", mux)
+  log.Fatal(err)
+}
